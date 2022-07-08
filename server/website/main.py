891cd7eb5b60cf05c3d3ba0a1eaf52e26e2e3f2c
Madhav Goyal <57744109+MadhavGoyal-6@users.noreply.github.com>
2022-07-08 19:09:06 +0530

    integrate pytorch MNIST model 🧠

--- server/website/main.py
+++ server/website/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask_sock import Sock
+import torch
+from model import net
 
 app = Flask(__name__)
 sock = Sock(app)
@@ -9,7 +11,12 @@ sock = Sock(app)
 def echo(sock):
     while True:
         data = sock.receive()
-
+        data = data.replace("[", "").replace("]", "")
+        data = data.split(",")
+        data = [int(i) for i in data]
+        data = torch.Tensor(data)
+        with torch.no_grad():
+            sock.send(net(data).tolist())
 
 @app.route("/")
 def home():
